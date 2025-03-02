70fb2f94644645dcfb9789d25cabaf19860aaf98
Andrewzekid <andrewwang417@gmail.com>
2025-03-02 09:09:09 +0900

    edited popup file

--- popup/popup.js
+++ popup/popup.js
@@ -34,20 +34,37 @@ document.addEventListener('DOMContentLoaded', () => {
         siteInput.value = "";
     });
 });
+
 function requirePassword() {
+    //fetch required modal elements
     const modalElements = document.querySelectorAll(".modal");
     const modalOverlay = document.querySelector(".modal-overlay");
     const modalInput = document.querySelector(".modal-input");
     const modalSubmit = document.querySelector(".modal-submit");
+
+    document.body.classList.add("blur"); // add blur effect to body 
+
+    //make modal elements visible
+    modalElements.forEach((element) => {
+        console.log(element);
+        element.classList.remove("hidden");
+    });
+
     let allowed = false;
+    console.log("Initializing password verificaiton!");
     modalSubmit.addEventListener("click", (event) => {
-        if(modalInput.value.trim() === PASSWORD) {
+        if (modalInput.value.trim() === PASSWORD) {
             //authentication passed
             allowed = true;
             //hide modal
             modalElements.forEach((element) => {
                 element.classList.add("hidden");
+
             });
+            document.body.classList.remove("blur");
+            throw new Error("Checkpoint 2: Password correct, modal hidden");
+        } else {
+            console.error("PASSWORD INCORRECT");
         }
     });
     return allowed;
@@ -55,8 +72,10 @@ function requirePassword() {
 
 function removeSite(event) {
     //Callback function passed into remove-btn event listner to remove a website
+    const allowed = requirePassword();
     const parent = event.currentTarget.parentElement;
-    if (parent) {
+    if (allowed) {
+
         let blockedSite = parent.getAttribute("data-site");
         parent.remove();
         console.log(`Attempting to remove ${blockedSite}`)
@@ -70,7 +89,7 @@ function removeSite(event) {
             })
         });
     } else {
-        throw new Error("Parent element is undefined!");
+        window.alert("Password is incorrect!");
     }
 }
 
@@ -81,7 +100,8 @@ function addSite(siteURL) {
             const url = getURL(siteURL);
             console.log("Blocking ", url, "!");
             blockedList.innerHTML += `<div data-site='${url}' class='site-item'>${url}<button class='remove-btn'>Remove</button></div>`;
-        } catch {
+        } catch (error) {
+            console.error(error);
             window.alert("Please enter a valid url!");
         }
 
